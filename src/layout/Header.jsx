35c3d5ff03c746375866052fdb3f62ffff90b452
Ulises Tejerina <u.tejerinachpque.17@etec.uba.ar>
2021-11-01 12:03:27 -0300

    Ultima actualizacion

---
+++ src/layout/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+import { FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+const Header = () => {
+  return (
+    <header>
+    <div id="contenedorBuscadorHeader">
+		 <input type="text" name="buscador" id="buscador" placeholder="Buscar"  />
+         <FontAwesomeIcon icon={faSearch} id="Iconobuscador" /> 
+    </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/layout/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Main.css";
+
+const Main = () => {
+ return (
+ <div>
+     SeccionPrincipal
+ </div>
+ 
+);
+
+};
+
+export default Main;
